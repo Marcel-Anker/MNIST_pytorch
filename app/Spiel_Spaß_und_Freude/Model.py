357a1ecd5b301dfb7c421b5526bb04b2505f52a9
Marcel Anker <M.Anker@wertgarantie.com>
2025-08-13 10:30:05 +0200

    add simple model to nn, flexable hidden layer with only fully connected layers

---
+++ app/Spiel_Spaß_und_Freude/Model.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+import torch.nn.functional as functions
+
+class Model(nn.Module):
+    def __init__(self, config):
+        super(Model, self).__init__()
+        self.config = config
+        self.firstFc = nn.Linear(28 * 28, self.config.hidden_layer_size) #28 * 28 mnist bild
+        for i in range(self.config.number_of_hidden_layers):
+            setattr(self, f"fc{i}", nn.Linear(self.config.hidden_layer_size, self.config.hidden_layer_size))
+        self.lastFc= nn.Linear(self.config.hidden_layer_size, 10) #0-9 als prediction
+
+    def forward(self, result):
+        result = result.view(result.size(0), -1)
+        result = functions.relu(self.firstFc(result))
+        for i in range(self.config.number_of_hidden_layers):
+            result = getattr(self, f"fc{i}")(result)
+        result = self.lastFc(result)
+        return result
