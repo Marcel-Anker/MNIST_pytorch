b1f13261a9912b0091b0524e7885e20207daf008
Marcel Anker <M.Anker@wertgarantie.com>
2025-08-13 10:29:24 +0200

    add data class to test nn, loading and formating data. validation splitting

---
+++ app/Spiel_Spaß_und_Freude/Data.py
@@ -0,0 +1,27 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+class Data:
+    def __init__(self, config):
+        self.config = config
+        self.transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))
+        ])
+
+    def get_loaders(self):
+        train_data = datasets.MNIST(root=self.config.data_root, train=True, download=True, transform=self.transform)
+        test_data = datasets.MNIST(root=self.config.data_root, train=False, download=True, transform=self.transform)
+
+        train_len = int(len(train_data) * 0.8)
+        val_len = len(train_data) - train_len
+
+        train_data, val_data = random_split(train_data, [train_len, val_len])
+
+        train_loader = DataLoader(dataset=train_data, batch_size=self.config.batchsize, shuffle=True)
+        val_loader = DataLoader(dataset=val_data, batch_size=self.config.batchsize, shuffle=True)
+        test_loader = DataLoader(dataset=test_data, batch_size=self.config.batchsize, shuffle=True)
+
+        return train_loader, val_loader, test_loader
