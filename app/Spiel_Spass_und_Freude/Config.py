a3e80fc88cbef70f3249178c0a13b674b49c65d5
Marcel Anker <M.Anker@wertgarantie.com>
2025-08-14 09:09:45 +0200

    delete unnecessary MLP and Config file

--- app/Spiel_Spass_und_Freude/Config.py
+++
@@ -1,9 +0,0 @@
-
-class Config:
-    def __init__(self, batchsize, lr, epochs, data_root, hidden_layer_size, number_of_hidden_layers):
-        self.batchsize = batchsize
-        self.learning_rate = lr
-        self.epochs = epochs
-        self.data_root = data_root
-        self.hidden_layer_size = hidden_layer_size
-        self.number_of_hidden_layers = number_of_hidden_layers
