49e166ec240f63c4575875b365f6674d598e7fb0
Marcel Anker <M.Anker@wertgarantie.com>
2025-09-10 16:18:59 +0200

    split functions for better overview

--- app/Trainer.py
+++ app/Trainer.py
@@ -3,18 +3,32 @@ from typing import Union
 import torch.nn
 from app.MLP.MLPConfig import MLPConfig
 from app.CNN.CNNConfig import CNNConfig
-from app.CNN.CNNModel import CNNModel
+from app.CNN.CNNBNormModel import CNNBNormModel
 from app.MLP.MLPModel import MLPModel
-import app.MLP.MLPConfig as a
+from app.TrainingMetric import TrainingMetric
+from app.Metrics import Metrics
+from app.CNN.CNN import CNN
+from app.MLP.MLP import MLP
+
 
 class Trainer:
     def __init__(self, model, config):
-        self.model: Union[CNNModel, MLPModel] = model
+        self.model: Union[MLPModel, CNNBNormModel] = model
         self.config: Union[MLPConfig, CNNConfig] = config
         self.loss_function = torch.nn.CrossEntropyLoss()
         self.optimizer = torch.optim.SGD(model.parameters(), lr=self.config.learning_rate, momentum=0.9)
 
-    def train(self, train_loader, val_loader):
+    def train(self, train_loader, val_loader) -> tuple[Metrics, Metrics]:
+
+            valMetrics, trainMetrics = self.runEpochs(train_loader, val_loader)
+
+            return valMetrics, trainMetrics
+
+    def runEpochs(self, train_loader, val_loader) -> tuple[Metrics, Metrics]:
+        best_val_loss = 2.0 #hoher Wert damit early Stopping ansetzt
+        counter = 0
+        trainMetrics = Metrics()
+        valMetrics = Metrics()
         for epoch in range(self.config.epochs):
             self.model.train()
             running_loss = 0.0
