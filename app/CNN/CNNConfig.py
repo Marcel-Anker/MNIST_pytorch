bf26220f6b1669bf3b27eab0e31310d9928f7b6e
Marcel Anker <M.Anker@wertgarantie.com>
2025-09-10 16:02:11 +0200

    edit cnnconfig to match defined hyperparameters as well as patience and epochs

--- app/CNN/CNNConfig.py
+++ app/CNN/CNNConfig.py
@@ -1,9 +1,12 @@
-class CNNConfig:
-    def __init__(self, batchsize, lr, epochs, number_convolution_layer, number_pooling_layer, kernel_size, padding):
+from pydantic import BaseModel
+
+class CNNConfig():
+    def __init__(self, batchsize, lr, number_conv_layers, kernel_size, conv_stride, epochs, patience, out_channels = 4):
         self.batchsize = batchsize
         self.learning_rate = lr
         self.epochs = epochs
-        self.number_convolution_layer = number_convolution_layer
-        self.number_pooling_layer = number_pooling_layer
+        self.number_conv_layers = number_conv_layers - 1
         self.kernel_size = kernel_size
-        self.padding = padding
+        self.out_channels = out_channels
+        self.conv_stride = conv_stride
+        self.patience = patience
