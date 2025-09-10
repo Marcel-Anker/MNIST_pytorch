41e5770fbd019a4a69dcdb914a17c915602ce80b
Marcel Anker <M.Anker@wertgarantie.com>
2025-09-10 15:59:23 +0200

    rename class to cnnbnormmodel and add bnorm to layers. implemented basic cnn with dynamic conv layers and one fc layer at the end

---
+++ app/CNN/CNNBNormModel.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch.nn.functional as functions
+
+class CNNBNormModel(nn.Module):
+    def __init__(self, config):
+        super(CNNBNormModel, self).__init__()
+        self.config = config
+        self.firstConv = nn.Conv2d(in_channels=1, out_channels=self.config.out_channels, kernel_size=self.config.kernel_size)
+        self.firstBNorm = nn.BatchNorm2d(self.config.out_channels)
+        h, w = self.compute_out_dim(height=28, width=28, kernel_size=self.config.kernel_size, stride=self.config.conv_stride, padding=0, num_layers=self.config.number_conv_layers)
+        for i in range(self.config.number_conv_layers):
+            setattr(self, f"conv{i}", nn.Conv2d(in_channels=self.config.out_channels, out_channels=self.config.out_channels * 2 * (i + 1), kernel_size=self.config.kernel_size))
+            setattr(self, f"bNorm{i}", nn.BatchNorm2d(self.config.out_channels * 2 * (i + 1)))
+            h, w = self.compute_out_dim(height=h, width=w, kernel_size=self.config.kernel_size, stride=self.config.conv_stride,padding=0, num_layers=self.config.number_conv_layers)
+        self.fc = nn.Linear(in_features=h * w * self.config.out_channels * 2 * self.config.number_conv_layers, out_features=10)
+
+    def forward(self, result):
+        result = result.view(result.size(0), -1)
+        result = functions.relu(self.firstFc(result))
+        for i in range(self.config.number_of_hidden_layers):
+            result = getattr(self, f"fc{i}")(result)
+        result = self.lastFc(result)
+        return result
