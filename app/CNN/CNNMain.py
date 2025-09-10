9541889106cd9f1ea734e515a38b6e3018b58203
Marcel Anker <M.Anker@wertgarantie.com>
2025-09-10 16:04:02 +0200

    keep file for single executions of specific hyperparameters

---
+++ app/CNN/CNNMain.py
@@ -0,0 +1,19 @@
+from CNNConfig import CNNConfig
+from app.Data import Data
+from CNNBNormModel import CNNBNormModel
+from app.Trainer import Trainer
+
+if __name__ == "__main__":
+    config = CNNConfig(batchsize=16, lr=0.002, number_conv_layers=2, kernel_size=3, out_channels=4, conv_stride=1)
+
+    data_module = Data(config)
+    train_loader, val_loader, test_loader = data_module.get_loaders()
+
+    modelBNorm = CNNBNormModel(config)
+
+    trainerBNorm = Trainer(modelBNorm, config)
+
+    trainerBNorm.train(train_loader, val_loader)
+    test_acc_bnorm = trainerBNorm.evaluate(test_loader)
+
+    print(f"Final BNorm Test Accuracy: {test_acc_bnorm:.2f}%")
