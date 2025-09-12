3ab236c6006d89319d9de204dc72173b8b2049e1
Marcel Anker <M.Anker@wertgarantie.com>
2025-09-12 11:24:31 +0200

    add patience to mlp config for early stopping

--- app/MLP/MLPConfig.py
+++ app/MLP/MLPConfig.py
@@ -1,8 +1,9 @@
 
 class MLPConfig:
-    def __init__(self, batchsize, lr, hidden_layer_size, number_of_hidden_layers, epochs = 15):
+    def __init__(self, batchsize, lr, hidden_layer_size, number_of_hidden_layers, epochs = 40, patience = 7):
         self.batchsize = batchsize
         self.learning_rate = lr
         self.epochs = epochs
         self.hidden_layer_size = hidden_layer_size
         self.number_of_hidden_layers = number_of_hidden_layers
+        self.patience = patience
