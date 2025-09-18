dc4edbd10d5ffc83f49f0ecdd1226f834f9355ef
Marcel Anker <M.Anker@wertgarantie.com>
2025-09-18 14:04:51 +0200

    rename number of kernels to number of conv layers

--- app/CNN/CNN.py
+++ app/CNN/CNN.py
@@ -4,12 +4,13 @@ from pydantic import BaseModel
 class CNN(BaseModel):
     lr: float
     batchsize: int
-    number_of_kernels: int
+    number_of_conv_layers: int
     kernel_size: int
     stride: int
 
-    def print(self) -> None:
+    def printParameters(self):
         print(
-            f"Learning Rate: {self.lr} | Batchsize: {self.batchsize} | Number of Conv Layers: {self.number_of_kernels} | "
+            f"These scores were achieved with the following parameters: "
+            f"Learning Rate: {self.lr} | Batchsize: {self.batchsize} | Number of Conv Layers: {self.number_of_conv_layers + 1} | "
             f"Kernel size: {self.kernel_size} | Stride: {self.stride}"
         )
--- app/MLP/MLP.py
+++ app/MLP/MLP.py
@@ -9,7 +9,8 @@ class MLP(BaseModel):
     hidden_layer_size: int
     number_of_hidden_layers: int
 
-    def print(self) -> None:
+    def printParameters(self):
         print(
+            f"These scores were achieved with the following parameters: "
             f"Learning Rate: {self.lr} | Batchsize: {self.batchsize} | Hidden Layer Size: {self.hidden_layer_size} | "
-            f"Number of Hidden Layers: {self.number_of_hidden_layers}")
+            f"Number of Hidden Layers: {self.number_of_hidden_layers + 1}")
