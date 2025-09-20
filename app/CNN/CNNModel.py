fix computation of feature map size

app/CNN/CNNModel.py
@@ -31,7 +31,7 @@ class CNNModel(nn.Module):
     def compute_out_dim(self, height, width, kernel_size, stride, padding, num_layers):
         h, w = height, width
         after_conv_h, after_conv_w = 0, 0
-        for _ in range(num_layers):
+        for _ in range(num_layers + 1):
             after_conv_h = (h + 2 * padding - kernel_size) // stride + 1
             after_conv_w = (w + 2 * padding - kernel_size) // stride + 1
 