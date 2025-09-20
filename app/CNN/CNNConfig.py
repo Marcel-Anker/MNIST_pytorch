38de221789d5945a786fbba56f5d1dae69981874
Marcel Anker <M.Anker@wertgarantie.com>
2025-09-20 19:48:10 +0200

    add printing methods to configs

--- app/CNN/CNNConfig.py
+++ app/CNN/CNNConfig.py
@@ -1,6 +1,7 @@
-from pydantic import BaseModel
+
 
 class CNNConfig():
+
     def __init__(self, batchsize, lr, number_conv_layers, kernel_size, conv_stride, epochs=40, patience=7, out_channels = 4):
         self.batchsize = batchsize
         self.learning_rate = lr
@@ -10,3 +11,10 @@ class CNNConfig():
         self.out_channels = out_channels
         self.conv_stride = conv_stride
         self.patience = patience
+
+    def printParameters(self):
+        print(
+            f"These scores were achieved with the following parameters: "
+            f"Learning Rate: {self.learning_rate} | Batchsize: {self.batchsize} | Number of Conv Layers: {self.number_conv_layers + 1} | "
+            f"Kernel size: {self.kernel_size} | Stride: {self.conv_stride}"
+        )
--- app/MLP/MLPConfig.py
+++ app/MLP/MLPConfig.py
@@ -1,5 +1,8 @@
 
+
+
 class MLPConfig:
+
     def __init__(self, batchsize, lr, hidden_layer_size, number_of_hidden_layers, epochs = 40, patience = 7):
         self.batchsize = batchsize
         self.learning_rate = lr
@@ -7,3 +10,9 @@ class MLPConfig:
         self.hidden_layer_size = hidden_layer_size
         self.number_of_hidden_layers = number_of_hidden_layers - 1
         self.patience = patience
+
+    def printParameters(self):
+        print(
+            f"These scores were achieved with the following parameters: "
+            f"Learning Rate: {self.learning_rate} | Batchsize: {self.batchsize} | Hidden Layer Size: {self.hidden_layer_size} | "
+            f"Number of Hidden Layers: {self.number_of_hidden_layers + 1}")
